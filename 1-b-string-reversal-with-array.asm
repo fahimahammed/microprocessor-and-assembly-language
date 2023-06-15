18d25c0bb7fefb3d73c1ba35d867990f4fe9fc3b
fahimahammed <fahimahammed.cse@gmail.com>
2023-06-15 22:35:17 +0600

    revision

--- 1-b-string-reversal-with-array.asm
+++ 1-b-string-reversal-with-array.asm
@@ -1,59 +1,59 @@
 .MODEL SMALL
 .STACK 100H
-
-.DATA
-INPUT_STR DB 100 DUP ('$')
+.DATA 
+MYSTR DB 100 DUP ('$')
 INPUT_MSG DB 'ENTER YOUR STRING: $'
-OUTPUT_MSG DB 0AH, 0DH, 'OUTPUT STRING: $'
+OUTPUT_MSG DB 0DH, 0AH, 'OUTPUT STRING: $'
 
 .CODE
 MAIN PROC
-    MOV AX, @DATA
-    MOV DS, AX 
+    MOV AX, @DATA 
+    MOV DS, AX
     
+    ; SHOW INPUT MSG
     MOV AH, 9
     LEA DX, INPUT_MSG
-    INT 21H
+    INT 21H  
     
-    ;TAKE INPUT
+    ;TAKE INPUT 
     MOV AH, 1
     XOR SI, SI
-    ;XOR CX, CX
-    
-    INPUT: 
-        INT 21H
+    INPUT:
+        INT 21H 
+        
         CMP AL, 0DH
         JE EXIT_LOOP
         
-        MOV INPUT_STR[SI], AL
-        ;INC CX
+        MOV MYSTR[SI], AL
         INC SI
         
         JMP INPUT
-    EXIT_LOOP: 
+    EXIT_LOOP:
     
+    ; SHOW OUTPUT MSG
     MOV AH, 9
     LEA DX, OUTPUT_MSG
     INT 21H
     
     DEC SI
     MOV AH, 2
-    LOOP_THROUGH_STR:
-        MOV DL, INPUT_STR[SI]
+    
+    LOOP_THROUGH_MYSTR:
+        MOV DL, MYSTR[SI]
         INT 21H
         
         CMP SI, 00H
         JE EXIT_FROM_LOOP
         
-        DEC SI
         
-        LOOP LOOP_THROUGH_STR
-    EXIT_FROM_LOOP:
-     
+        DEC SI
     
+    
+    LOOP LOOP_THROUGH_MYSTR
+    EXIT_FROM_LOOP:
     
     MOV AH, 4CH
     INT 21H
     
-ENDP MAIN
+    MAIN ENDP
 END MAIN
