495ef17a742dcc7edc18b6d2375b34da338a4a4a
fahimahammed <fahimahammed.cse@gmail.com>
2023-06-15 22:44:17 +0600

    revision

--- 1-b-string-reversal-with-stact.asm
+++ 1-b-string-reversal-with-stact.asm
@@ -1,20 +1,18 @@
 .MODEL SMALL
-.STACK 100H  
-
+.STACK 100H
 .DATA
-INPUT_STR DB 100 DUP ('$')
 INPUT_MSG DB 'ENTER YOUR STRING: $'
-OUTPUT_MSG DB 0AH, 0DH, 'OUTPUT STRING: $'
-  
+OUTPUT_MSG DB 0DH, 0AH, 'OUTPUT STRING: $' 
 
 .CODE
-MAIN PROC 
+MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
     
+    ;SHOW INPUT MSG
     MOV AH, 9
     LEA DX, INPUT_MSG
-    INT 21H 
+    INT 21H
     
     XOR SI, SI
     MOV AH, 1
@@ -22,35 +20,37 @@ MAIN PROC
         INT 21H
         
         CMP AL, 0DH
-        JE EXIT_FROM_LOOP
+        JE EXIT_FROM_INPUT
         
         PUSH AX
-        INC SI
-        
-    JMP INPUT
-    EXIT_FROM_LOOP: 
+        INC SI 
     
+    JMP INPUT 
+    EXIT_FROM_INPUT:
+    
+    ; SHOW OUTPUT MSG
     MOV AH, 9
     LEA DX, OUTPUT_MSG
-    INT 21H
+    INT 21H 
+    
     
     DEC SI
     MOV AH, 2
-    
-    OUTPUT:
+    LOOP_THROUGH_MYSTR:
         POP DX
         INT 21H
         
         CMP SI, 00H
-        JE EXIT_OUTPUT
+        JE EXIT_MYSTR_LOOP
         
         DEC SI
-    LOOP OUTPUT
-    EXIT_OUTPUT:
     
+    LOOP LOOP_THROUGH_MYSTR
+    EXIT_MYSTR_LOOP:
     
     
     MOV AH, 4CH
     INT 21H
-ENDP MAIN
+    
+    MAIN ENDP
 END MAIN
